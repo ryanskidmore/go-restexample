8a83afee259ace6c01db2e89d70679f672a7fc67
Ryan Skidmore <ryanskidmore1997@googlemail.com>
2019-03-09 00:01:11 +0000

    Exported Struct Fields, Added Get User or Error func

--- user.go
+++ user.go
@@ -4,12 +4,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Users map[string]*User
+
 type User struct {
-	id    string
-	email string
-	name  string
+	Id    string
+	Email string
+	Name  string
 }
 
 func GetCertificatesHandler(c *gin.Context) {
 
+}
+
+func GetUserOrError(c *gin.Context) *User {
+	Session := c.Request.Header.Get("Authorization")
+	if Session == "" {
+		c.AbortWithStatus(401)
+		return nil
+	}
+	if UserID, Exists := Sessions[Session]; Exists {
+		return Users[UserID]
+	} else {
+		c.AbortWithStatus(401)
+		return nil
+	}
 }
