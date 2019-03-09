4a2e6cc0f75198eaa4213ce4d06d6fef73b01fc7
Ryan Skidmore <ryanskidmore1997@googlemail.com>
2019-03-09 00:01:29 +0000

    Add Dummy Data Generation

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var Sessions map[string]string
+
 func main() {
+	InitDummyData()
 	router := gin.Default()
 	router.POST("/certificates", NewCertificateHandler)
 	router.PUT("/certificates/:id", UpdateCertificateHandler)
@@ -13,4 +16,22 @@ func main() {
 	router.POST("/certificates/:id/transfers", CreateTransferHandler)
 	router.PUT("/certificates/:id/transfers", AcceptTransferHandler)
 	router.Run()
+}
+
+func InitDummyData() {
+	Certificates = make(map[string]*Certificate)
+	Users = make(map[string]*User)
+	Sessions = make(map[string]string)
+	Users["db75530b-34d4-47df-bb08-82f075b6045b"] = &User{
+		Id:    "db75530b-34d4-47df-bb08-82f075b6045b",
+		Email: "bob@examplemail.com",
+		Name:  "Bob",
+	}
+	Users["11150398-01a1-4335-afde-4cbaa5d490f9"] = &User{
+		Id:    "11150398-01a1-4335-afde-4cbaa5d490f9",
+		Email: "alice@examplemail.com",
+		Name:  "Alice",
+	}
+	Sessions["2b65701217e2c546bbf69a982c50014d503b977be64b7687616858ef8b6ed411"] = "db75530b-34d4-47df-bb08-82f075b6045b"
+	Sessions["9609301053e5cb2b2e43258e163120a70f420adfacda30c5d8d1e069ae02f028"] = "11150398-01a1-4335-afde-4cbaa5d490f9"
 }
