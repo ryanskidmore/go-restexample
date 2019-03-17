3c13d7981234469d7c1e86ba16aa176a4cdfe6bb
Ryan Skidmore <ryanskidmore1997@googlemail.com>
2019-03-17 13:02:10 +0000

    Documented unit tests

--- certificate_test.go
+++ certificate_test.go
@@ -10,50 +10,59 @@ import (
 )
 
 func TestNewCertificateHandler(t *testing.T) {
+	// Initialise api server
 	go main()
+	// Wait for server to start
 	time.Sleep(5 * time.Second)
-	Certificates = make(map[string]*Certificate)
+	// Create new certificate
 	TestCert := Certificate{
 		Title: "Example Work",
 		Year:  1950,
 		Note:  "Example Note",
 	}
+	// Convert new cert into JSON
 	TestCertJSON, err := json.Marshal(TestCert)
 	if err != nil {
 		t.Error("Failed to create new certificate, error during JSON marshal: " + err.Error())
 		return
 	}
+	// Create new request to POST/create certificate
 	Request, err := http.NewRequest("POST", "http://localhost:8080/certificates", bytes.NewBuffer(TestCertJSON))
 	if err != nil {
 		t.Error("Failed to create new certificate, error while creating new request: " + err.Error())
 		return
 	}
+	// Set appropiate headers
 	Request.Header.Set("Authorization", "2b65701217e2c546bbf69a982c50014d503b977be64b7687616858ef8b6ed411")
 	Request.Header.Set("Content-Type", "application/json")
 	Client := &http.Client{}
+	// Perform request
 	Response, err := Client.Do(Request)
 	if err != nil {
 		t.Error("Failed to create new certificate, error while performing request: " + err.Error())
 		return
 	}
 	defer Response.Body.Close()
-
+	// Read response
 	RespBody, err := ioutil.ReadAll(Response.Body)
 	if err != nil {
 		t.Error("Failed to create new certificate, error while reading response: " + err.Error())
 		return
 	}
+	// Convert response into map
 	var RespMap map[string]interface{}
 	err = json.Unmarshal(RespBody, &RespMap)
 	if err != nil {
 		t.Error("Failed to create new certificate, error while decoding response: " + err.Error())
 		return
 	}
+	// Check if certificate creation was successful
 	CertStatus := RespMap["status"].(string)
 	if CertStatus == "failed" {
 		t.Error("Failed to create new certificate: " + RespMap["error"].(string))
 		return
 	}
+	// Check cert exists
 	CertID := RespMap["id"].(string)
 	if Cert, Exists := Certificates[CertID]; Exists {
 		if Cert.Title == TestCert.Title && Cert.Year == TestCert.Year && Cert.Note == TestCert.Note {
